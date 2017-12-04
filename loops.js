b25f416a595c5b68849d48cf121e57c37759f8e7
Starlight Romero <starlightromero@gmail.com>
2017-12-04 17:47:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-    for (array = 0; array < 25; array++) {
-      console.log('I am ' + array + ' strange loops.')
+    for (let i = 0; i < 25; i++) {
+      console.log('I am ' + i + ' strange loops.')
     }
 }
